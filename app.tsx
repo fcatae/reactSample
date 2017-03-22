fbe480e38c20fb7522ec467a582b4d4cce9100b5
Fabricio Catae <fcatae@microsoft.com>
2017-03-22 20:57:01 -0300

    add color styles

--- app.tsx
+++ app.tsx
@@ -3,7 +3,8 @@ class App extends React.Component<{},{}> {
       return (
          <div>
             <Word text="Hello"/>
-            <Word text="Word"/>
+            <Word text="Word" color="color-orange"/>
+            <Word text="1"/>
          </div>
       );
    }
@@ -11,14 +12,20 @@ class App extends React.Component<{},{}> {
 
 interface IWord {
     text: string;
+    color?: string;
 }
 
 class Word extends React.Component<IWord,{}> {
-   render() {
+
+    toColor() {
+        return (this.props.color) ? this.props.color : "color-none";
+    }
+
+    render() {
       return (
-         <div>
+         <span className={"word " + this.toColor()}>
             {this.props.text}
-         </div>
+         </span>
       );
    }
 }
