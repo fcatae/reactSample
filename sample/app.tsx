508677b2587e93c969799fb57bd0486816f96e87
Fabricio Catae <fcatae@microsoft.com>
2017-03-22 20:23:59 -0300

    move to sample

---
+++ sample/app.tsx
@@ -0,0 +1,19 @@
+class App extends React.Component<{},{}> {
+   render() {
+      return (
+         <div>
+            <Words/>
+         </div>
+      );
+   }
+}
+
+class Words extends React.Component<{},{}> {
+   render() {
+      return (
+         <div>
+            Hello Word!!!
+         </div>
+      );
+   }
+}
