4d9c2241c3bf9be4ad8f5fa4bb7cf152fd0a8b36
Fabricio Catae <fcatae@microsoft.com>
2017-03-22 21:15:24 -0300

    use switch case logic

--- app.tsx
+++ app.tsx
@@ -1,24 +1,56 @@
+
 class App extends React.Component<{},{}> {
    render() {
       return (
          <div>
             <Word text="Hello"/>
-            <Word text="Word" color="color-orange"/>
-            <Word text="1"/>
+            <Word text="Word" wordState={WordState.Highlight} />
+            <Word text="1" wordState={WordState.Ignore} />
          </div>
       );
    }
 }
 
+class Phrase extends React.Component<IWord,{}> {
+
+    render() {
+      return (
+         <div>
+            <Word text="Hello"/>
+            <Word text="Word" wordState={WordState.Highlight} />
+            <Word text="1" wordState={WordState.Ignore} />
+         </div>
+      );
+   }
+}
+
+
+enum WordState {
+    Normal = 0,
+    Highlight,
+    Ignore
+}
+
 interface IWord {
     text: string;
-    color?: string;
+    wordState?: WordState;
 }
 
 class Word extends React.Component<IWord,{}> {
 
     toColor() {
-        return (this.props.color) ? this.props.color : "color-none";
+        let state = (this.props.wordState) ? this.props.wordState : WordState.Normal;
+
+        switch(state) {
+            case WordState.Normal:
+                return 'color-normal';
+            case WordState.Ignore:
+                return 'color-none';
+            case WordState.Highlight:
+                return 'color-highlight';                
+        }
+
+        throw "Invalid color";
     }
 
     render() {
